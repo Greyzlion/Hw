5f570896a85bce5339486cba405e564d3f0f5192
Your Greyzlion <hamidsha@code.edu.az>
2022-02-14 04:59:36 +0400

    Hw

---
+++ Task1/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace task1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double exam1 = 0, exam2 = 0;
+            double average = (exam1 + exam2) / 2;
+            Console.WriteLine("Birinci imtahani daxil edin");
+        first:
+
+         exam1 = Convert.ToDouble(Console.ReadLine());
+        second:
+            Console.WriteLine("Ikinci imtahan neticesini daxil edin:");
+
+            exam2 = Convert.ToDouble(Console.ReadLine());
+            CalcAvg(average);
+        }
+        static void CalcAvg(double average)
+        {
+            if (average >= 60)
+            {
+                Console.WriteLine("Mezun oldunuz");
+            }
+            else
+            {
+                Console.WriteLine("Mezun ola bilmediniz");
+            }
+
+
+        }
+    }
+        
+}
